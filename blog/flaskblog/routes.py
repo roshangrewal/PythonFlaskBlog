40c417c05f7be94b2aa3cbdb0d1a32c865cfbb21
roshangrewal <roshangrewal@hotmail.com>
2019-04-16 20:21:12 +0530

    Comment

--- blog/flaskblog/routes.py
+++ blog/flaskblog/routes.py
@@ -8,6 +8,7 @@ from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
+#Comment
 posts = [
     {
         'author': 'Corey Schafer',
