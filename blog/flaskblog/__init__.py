9722b1bf2b4c9d31e8806d3ebb0c3b9d55f08fdd
roshangrewal <roshangrewal@hotmail.com>
2019-04-17 18:36:59 +0530

    Password reset module done

--- blog/flaskblog/__init__.py
+++ blog/flaskblog/__init__.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '408f3a217a2ad629a63ed6c7d26e1c07'
@@ -12,5 +13,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'iconiixsoftware@gmail.com'
+app.config['MAIL_PASSWORD'] = ''
+mail = Mail(app)
 
 from flaskblog import routes
