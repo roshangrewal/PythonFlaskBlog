02375aac31515b83e4b30e5a0bf2bfa46a0a647b
roshangrewal <roshangrewal@hotmail.com>
2019-06-14 21:28:01 +0530

    requirements dot txt > updated

--- blog/flaskblog/__init__.py
+++ blog/flaskblog/__init__.py
@@ -17,7 +17,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'iconiixsoftware@gmail.com'
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_PASSWORD'] = 'icon22125'
 mail = Mail(app)
 
 from flaskblog import routes
